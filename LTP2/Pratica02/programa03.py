d92ac5e7e952f44a032992dcf7aa9e4027363f67
repeiras <69979166+repeiras@users.noreply.github.com>
2020-08-27 10:34:52 -0300

    Create programa03.py

---
+++ LTP2/Pratica02/programa03.py
@@ -0,0 +1,7 @@
+#Usuario fala 2 numeros e nós calculamos a media deles
+numero1 = int(input("numero1"))
+numero2 = int(input("numero2"))
+
+media = (numero1 + numero2) / 2
+
+print("o resultado da media:",media)
